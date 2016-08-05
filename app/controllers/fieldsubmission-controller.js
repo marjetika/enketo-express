16a5274e114d1959bca0bfa8cc3e9a17df0fe12b
Martijn van de Rijdt <martijn@enketo.org>
2016-08-05 17:04:54 -0600

    basic queuing mechanism + pass fieldchange event from form + submission scaffolding

---
+++ app/controllers/fieldsubmission-controller.js
@@ -0,0 +1,107 @@
+'use strict';
+
+var communicator = require( '../lib/communicator' );
+var surveyModel = require( '../models/survey-model' );
+var userModel = require( '../models/user-model' );
+//var instanceModel = require( '../models/instance-model' );
+//var submissionModel = require( '../models/submission-model' );
+var utils = require( '../lib/utils' );
+var request = require( 'request' );
+var express = require( 'express' );
+var router = express.Router();
+// var debug = require( 'debug' )( 'submission-controller' );
+
+module.exports = function( app ) {
+    app.use( app.get( 'base path' ) + '/fieldsubmission', router );
+};
+
+// duplicate in survey-controller
+router.param( 'enketo_id', function( req, res, next, id ) {
+    if ( /^::[A-z0-9]{4,8}$/.test( id ) ) {
+        req.enketoId = id.substring( 2 );
+        next();
+    } else {
+        next( 'route' );
+    }
+} );
+
+router
+    .all( '*', function( req, res, next ) {
+        res.set( 'Content-Type', 'application/json' );
+        next();
+    } )
+    .post( '/:enketo_id', submit )
+    .all( '/*', function( req, res, next ) {
+        var error = new Error( 'Not allowed' );
+        error.status = 405;
+        next( error );
+    } );
+
+/** 
+ * Simply pipes well-formed request to the OpenRosa server and
+ * copies the response received.
+ *
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+function submit( req, res, next ) {
+    var submissionUrl;
+    var credentials;
+    var options;
+    var paramName = req.app.get( 'query parameter to pass to submission' );
+    var paramValue = req.query[ paramName ];
+    var query = ( paramValue ) ? '?' + paramName + '=' + paramValue : '';
+    var instanceId = req.headers[ 'x-openrosa-instance-id' ];
+    var deprecatedId = req.headers[ 'x-openrosa-deprecated-id' ];
+    var id = req.enketoId;
+
+    res.status( 404 ).end();
+
+    /*surveyModel.get( id )
+        .then( function( survey ) {
+            submissionUrl = false // TODO communicator.getSubmissionUrl( survey.openRosaServer ) + query;
+            credentials = userModel.getCredentials( req );
+
+            // first check if authentication is required and if so get the Basic or Digest Authorization header
+            return communicator.getAuthHeader( submissionUrl, credentials );
+        } )
+        .then( function( authHeader ) {
+            options = {
+                url: submissionUrl,
+                headers: authHeader ? {
+                    'Authorization': authHeader
+                } : {}
+            };
+
+            // pipe the request 
+            req.pipe( request( options ) ).on( 'response', function( orResponse ) {
+                if ( orResponse.statusCode === 201 ) {
+                    _logSubmission( id, instanceId, deprecatedId );
+                } else if ( orResponse.statusCode === 401 ) {
+                    // replace the www-authenticate header to avoid browser built-in authentication dialog
+                    orResponse.headers[ 'WWW-Authenticate' ] = 'enketo' + orResponse.headers[ 'WWW-Authenticate' ];
+                }
+            } ).pipe( res );
+
+        } )
+        .catch( next );*/
+}
+
+/*
+function _logSubmission( id, instanceId, deprecatedId ) {
+    submissionModel.isNew( id, instanceId )
+        .then( function( notRecorded ) {
+            if ( notRecorded ) {
+                // increment number of submissions
+                surveyModel.incrementSubmissions( id );
+                // store/log instanceId
+                submissionModel.add( id, instanceId, deprecatedId );
+            }
+        } )
+        .catch( function( error ) {
+            console.error( error );
+        } );
+}
+*/
--- public/js/src/module/controller-webform-fieldsubmission.js
+++ public/js/src/module/controller-webform-fieldsubmission.js
@@ -7,13 +7,17 @@
 'use strict';
 
 var gui = require( './gui' );
-var connection = require( './connection' );
 var settings = require( './settings' );
 var Form = require( 'enketo-core' );
 var fileManager = require( './file-manager' );
 var t = require( './translator' ).t;
 var $ = require( 'jquery' );
-
+var utils = require( './utils' );
+var FIELDSUBMISSION_URL = ( settings.enketoId ) ? settings.basePath + '/fieldsubmission/' + settings.enketoIdPrefix + settings.enketoId +
+    utils.getQueryString( settings.submissionParameter ) : null;
+var fieldSubmissionQueue = [];
+var fieldSubmissionOngoing = false;
+var fieldSubmissionInterval;
 var form;
 var formSelector;
 var formData;
@@ -79,94 +83,174 @@ function _resetForm( confirmed ) {
             external: formData.external
         } );
         form.init();
-        // formreset event will update the form media:
-        form.getView().$.trigger( 'formreset' );
-        //if ( records ) {
-        //    records.setActive( null );
-        //}
+        form.getView().$
+            .trigger( 'formreset' );
     }
 }
 
 /**
- * Used to submit a form.
- * This function does not save the record in localStorage
- * and is not used in offline-capable views.
+ * Finishes a submission
  */
-function _submitRecordField() {
-    var record;
-    var redirect;
-    var beforeMsg;
-    var authLink;
-    var level;
-    var msg = [];
+function _finish( updated ) {
+    //var record;
+    //var redirect;
+    //var beforeMsg;
+    //var authLink;
+    //var level;
+    //var msg = [];
 
     form.getView().$.trigger( 'beforesave' );
 
-    beforeMsg = ( redirect ) ? t( 'alert.submission.redirectmsg' ) : '';
-    authLink = '<a href="/login" target="_blank">' + t( 'here' ) + '</a>';
+    //beforeMsg = ( redirect ) ? t( 'alert.submission.redirectmsg' ) : '';
+    //authLink = '<a href="/login" target="_blank">' + t( 'here' ) + '</a>';
 
-    gui.alert( beforeMsg + '<br />' +
-        '<div class="loader-animation-small" style="margin: 10px auto 0 auto;"/>', t( 'alert.submission.msg' ), 'bare' );
+    //gui.alert( beforeMsg + '<br />' +
+    //    '<div class="loader-animation-small" style="margin: 10px auto 0 auto;"/>', t( 'alert.submission.msg' ), 'bare' );
+    /*
+        record = {
+            'xml': form.getDataStr(),
+            'files': fileManager.getCurrentFiles(),
+            'instanceId': form.getInstanceID(),
+            'deprecatedId': form.getDeprecatedID()
+        };
 
-    record = {
-        'xml': form.getDataStr(),
-        'files': fileManager.getCurrentFiles(),
-        'instanceId': form.getInstanceID(),
-        'deprecatedId': form.getDeprecatedID()
-    };
+        return connection.uploadRecord( record )
+            .then( function( result ) {
+                result = result || {};
+                level = 'success';
 
-    return connection.uploadRecord( record )
-        .then( function( result ) {
-            result = result || {};
-            level = 'success';
+                if ( result.failedFiles && result.failedFiles.length > 0 ) {
+                    msg = [ t( 'alert.submissionerror.fnfmsg', {
+                        failedFiles: result.failedFiles.join( ', ' ),
+                        supportEmail: settings.supportEmail
+                    } ) ];
+                    level = 'warning';
+                }
 
-            if ( result.failedFiles && result.failedFiles.length > 0 ) {
-                msg = [ t( 'alert.submissionerror.fnfmsg', {
-                    failedFiles: result.failedFiles.join( ', ' ),
-                    supportEmail: settings.supportEmail
-                } ) ];
-                level = 'warning';
-            }
+                // this event is used in communicating back to iframe parent window
+                $( document ).trigger( 'submissionsuccess' );
 
-            // this event is used in communicating back to iframe parent window
-            $( document ).trigger( 'submissionsuccess' );
-
-            if ( settings.returnUrl ) {
-                msg += '<br/>' + t( 'alert.submissionsuccess.redirectmsg' );
-                gui.alert( msg, t( 'alert.submissionsuccess.heading' ), level );
-                setTimeout( function() {
-                    location.href = decodeURIComponent( settings.returnUrl );
-                }, 1500 );
-            } else {
-                msg = ( msg.length > 0 ) ? msg : t( 'alert.submissionsuccess.msg' );
-                gui.alert( msg, t( 'alert.submissionsuccess.heading' ), level );
-                _resetForm( true );
-            }
-        } )
-        .catch( function( result ) {
-            var message;
-            result = result || {};
-            console.error( 'submission failed', result );
-            if ( result.status === 401 ) {
-                message = t( 'alert.submissionerror.authrequiredmsg', {
-                    here: authLink
-                } );
-            } else {
-                message = result.message || gui.getErrorResponseMsg( result.status );
-            }
-            gui.alert( message, t( 'alert.submissionerror.heading' ) );
-        } );
+                if ( settings.returnUrl ) {
+                    msg += '<br/>' + t( 'alert.submissionsuccess.redirectmsg' );
+                    gui.alert( msg, t( 'alert.submissionsuccess.heading' ), level );
+                    setTimeout( function() {
+                        location.href = decodeURIComponent( settings.returnUrl );
+                    }, 1500 );
+                } else {
+                    msg = ( msg.length > 0 ) ? msg : t( 'alert.submissionsuccess.msg' );
+                    gui.alert( msg, t( 'alert.submissionsuccess.heading' ), level );
+                    _resetForm( true );
+                }
+            } )
+            .catch( function( result ) {
+                var message;
+                result = result || {};
+                console.error( 'submission failed', result );
+                if ( result.status === 401 ) {
+                    message = t( 'alert.submissionerror.authrequiredmsg', {
+                        here: authLink
+                    } );
+                } else {
+                    message = result.message || gui.getErrorResponseMsg( result.status );
+                }
+                gui.alert( message, t( 'alert.submissionerror.heading' ) );
+            } );
+        */
 }
 
+function _addToFieldSubmissionQueue( fieldPath, value, instanceId, deprecatedId ) {
+    var fd = new FormData();
 
+    if ( fieldPath && instanceId ) {
+        if ( value instanceof Blob ) {
+            fd.append( fieldPath, value, value.name );
+        } else {
+            fd.append( fieldPath, value );
+        }
+        fd.append( 'instanceID', instanceId );
+        if ( deprecatedId ) {
+            fd.append( 'deprecatedID', deprecatedId );
+        }
+        // TODO: check for existance of older field update???
+        fieldSubmissionQueue.push( fd );
+        console.debug( 'new fieldSubmissionQueue', fieldSubmissionQueue );
+    } else {
+        console.error( 'Attempt to add field submission without path or instanceID' );
+    }
+}
+
+function _submitFieldSubmissionQueue() {
+    if ( fieldSubmissionQueue.length && !fieldSubmissionOngoing ) {
+        console.debug( 'submitting queue' );
+        return fieldSubmissionQueue.reduce( function( prevPromise, fieldSubmission ) {
+                return prevPromise.then( function() {
+                    return _submitFieldSubmission( fieldSubmission );
+                } );
+            }, Promise.resolve() )
+            .then( function( results ) {
+                console.debug( 'all done with queue submission, results: ', results, ', current queue', fieldSubmissionQueue );
+            } )
+            .then( _restartSubmissionInterval );
+    }
+}
+
+function _submitFieldSubmission( fd ) {
+    return new Promise( function( resolve, reject ) {
+        $.ajax( FIELDSUBMISSION_URL, {
+                type: 'POST',
+                data: fd,
+                cache: false,
+                contentType: false,
+                processData: false,
+                headers: {
+                    'X-OpenClinica-Version': '1.0'
+                },
+                timeout: 3 * 60 * 1000
+            } )
+            .done( function( data, textStatus, jqXHR ) {
+                resolve( jqXHR.status );
+            } )
+            .fail( function() {
+                reject( 'Failed to connect with /fieldsubmission server.' );
+            } );
+    } );
+}
+
+function _restartSubmissionInterval() {
+    console.debug( 'restarting submissionInterval' );
+    fieldSubmissionInterval = setInterval( _submitFieldSubmissionQueue, 1 * 60 * 1000 );
+}
 
 function _setEventHandlers() {
     var $doc = $( document );
-    $doc.on( 'progressupdate.enketo', 'form.or', function( event, status ) {
-        if ( $formprogress.length > 0 ) {
-            $formprogress.css( 'width', status + '%' );
-        }
-    } );
+    $doc
+        .on( 'progressupdate.enketo', 'form.or', function( event, status ) {
+            if ( $formprogress.length > 0 ) {
+                $formprogress.css( 'width', status + '%' );
+            }
+        } )
+        .on( 'fieldchange.enketo', 'form.or', function( event, updated ) {
+            console.debug( 'fieldchange! ', updated );
+            var instanceId = form.getInstanceID();
+            var deprecatedId = form.getDeprecatedID();
+            if ( updated.fullPath && instanceId ) {
+                updated.valid
+                    .then( function( valid ) {
+                        if ( valid ) {
+                            if ( !deprecatedId ) {
+                                _addToFieldSubmissionQueue( updated.fullPath, updated.value, instanceId );
+                                _submitFieldSubmissionQueue();
+                            } else {
+                                // TODO handle PUT /fieldsubmissionupdates
+                            }
+                        } else {
+                            console.debug( 'value is not valid, will not submit' );
+                        }
+                    } );
+            } else {
+                console.error( 'Could not submit field. Missing either fullpath or instanceId', updated, instanceId );
+            }
+        } );
 
     if ( _inIframe() && settings.parentWindowOrigin ) {
         //$doc.on( 'submissionsuccess edited.enketo', _postEventAsMessageToParentWindow );
