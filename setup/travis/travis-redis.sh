75b4cbabf0cb1b9618455080ca0c8b0799f72b9a
Martijn van de Rijdt <martijn@enketo.org>
2018-07-25 17:14:03 -0600

    more clever

--- setup/travis/travis-redis.sh
+++ setup/travis/travis-redis.sh
@@ -2,10 +2,9 @@
 
 echo "Setting up a second redis instance on port 6380..."
 
-CONFIGPATH=${TRAVIS_BUILD_DIR}/setup/travis
-
-#sudo cp ${CONFIGPATH}/redis-enketo-cache-upstart.conf /etc/redis/redis-enketo-cache.conf
-sudo cp ${TRAVIS_BUILD_DIR}/setup/redis/redis-enketo-cache.conf /etc/redis/
-sudo cp ${CONFIGPATH}/redis-enketo-cache.conf /etc/init/
+# remove first lines from systemd configuration file
+sudo tail -n +5 ${TRAVIS_BUILD_DIR}/setup/redis/redis-enketo-cache.conf > /etc/redis/redis-enketo-cache.conf
+# copy upstart service file
+sudo cp ${TRAVIS_BUILD_DIR}/setup/travis/redis-enketo-cache.conf /etc/init/
 sudo start redis-enketo-cache
 sleep 3
