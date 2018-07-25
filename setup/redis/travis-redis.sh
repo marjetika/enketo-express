7db5c4dc728981b736da008503a152a0e7151bc1
Martijn van de Rijdt <martijn@enketo.org>
2018-07-25 16:48:51 -0600

    fix CI env?

--- setup/redis/travis-redis.sh
+++ setup/redis/travis-redis.sh
@@ -5,6 +5,6 @@ echo "Setting up a second redis instance on port 6380..."
 CONFIGPATH=${TRAVIS_BUILD_DIR}/setup/redis
 
 sudo cp ${CONFIGPATH}/conf/redis-enketo-cache-upstart.conf /etc/redis/redis-enketo-cache.conf
-sudo cp ${CONFIGPATH}/init/redis-server-enketo-cache.conf /etc/init/
-sudo start redis-server-enketo-cache
+sudo cp ${CONFIGPATH}/init/redis-enketo-cache.conf /etc/init/
+sudo start redis-enketo-cache
 sleep 3
